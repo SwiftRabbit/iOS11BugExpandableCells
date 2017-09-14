727b7bbb0c5fda87d11d77259a46af1c7b27e159
Léopold Roitel <leopold.roitel@intact.net>
2017-09-14 10:32:19 -0400

    Initial Commit

---
+++ iOS11BugExpandableCells/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS11BugExpandableCells
+//
+//  Created by Leopold Roitel on 17-09-14.
+//  Copyright © 2017 Leopold Roitel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
